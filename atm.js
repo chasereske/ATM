7e73353df18c07c55bd2759f6da2d6225ff73cfb
chasereske <chasereske@gmail.com>
2021-01-27 22:46:04 -0500

    I built out more of the functionality in the atm file, but need to clean up the index file some.

--- atm.js
+++ atm.js
@@ -1,24 +1,38 @@
 "use strict"
 
-const initialUserAccountInfo = require('./account')
+const initialUserAccountInfo = require('./account');
+const prompt = require('prompt-sync')();
+
+let userResponse;
+
+function userAttemptedPin () {
+    userResponse = prompt().parseINT;
+}
 
 function validatePin() {
+    if(userResponse === initialUserAccountInfo.userPin) {
+        userMenu();
+    } else {
+        console.log("That was incorrect. Please try again.");
+        console.log(userInput.userAttemptedPin);
+        userInput.userAttemptedPin; 
+    }
 
 }
 
-function getBalance() {
+function userMenu() { 
+    function getBalance() {
 
-}
+    }
 
-function withdraw() {
+    function withdraw() {
 
-}
+    }
 
-function deposit() {
+    function deposit() {
 
+    }
 }
 
 module.exports.validatePin = validatePin;
-module.exports.getBalance = getBalance;
-module.exports.withdraw = withdraw;
-module.exports.deposit = deposit;
+module.exports.userMenu = userMenu;
--- index.js
+++ index.js
@@ -3,8 +3,13 @@
  const prompt = require('prompt-sync')()
 
  const userOptions = require('./atm')
- 
+
  console.log("What is your pin number?");
 
- let userAttemptedPin = prompt();
+ let userAttemptedPin = prompt().parseINT;
+
+ userOptions.validatePin(userAttemptedPin);
+
+ module.exports.userAttemptedPin = userAttemptedPin;
+
 
